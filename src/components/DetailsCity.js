ec787f60a1d03440ae1b27eb980d081652cabdc8
medina192 <ja.diazmedina@ugto.mx>
2021-11-15 12:26:42 -0600

    errors corrected

--- src/components/DetailsCity.js
+++ src/components/DetailsCity.js
@@ -1,8 +1,8 @@
 import React, {useRef} from 'react';
 import { useNavigate} from "react-router-dom";
-import CityData from '../services/CityData';
+//import CityData from '../services/CityData';
 import ContainerIconClimate from './ContainerIconClimate';
-import { BsPercent } from "react-icons/bs";
+//import { BsPercent } from "react-icons/bs";
 
 const DetailsCity = () => {
 
